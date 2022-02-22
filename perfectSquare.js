b076fa3eb72be08fc64055872146d968618153ca
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-22 09:50:11 -0700

    got rid of worthless code

--- perfectSquare.js
+++ perfectSquare.js
@@ -2,9 +2,6 @@ function perfectSquare(num) {
     let squared = Math.sqrt(num) + 1
 
     if (Math.sqrt(num) % 1 === 0) {
-        while (squared % 1 !== 0) {
-            squared += 1
-        }
         console.log(squared * squared)
     } else {
         console.log(-1)
@@ -17,4 +14,4 @@ perfectSquare(289) // should return 324 (17x17=289 18x18=324)
 
 perfectSquare(3000) // should return -1 (sq root of 3000 is 54.77)
 
-perfectSquare(16)
+perfectSquare(10)
